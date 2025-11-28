afbc9aa0978f110886273d6de203a03cc9fcc06d
Lucas Alberto <lucasalberto4321@gmail.com>
2025-11-28 18:32:10 -0300

    feat: instalacao-do-AOS-animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+// @ts-ignore
+import AOS from 'aos';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +14,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  constructor(private router: Router) {}
+export class AppComponent implements OnInit {
+
+  protected readonly title = signal('GerenciadorDeClientes');
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 
   shouldShowHeader(): boolean {
     const currentUrl = this.router.url;
